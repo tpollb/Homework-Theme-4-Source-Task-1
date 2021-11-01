8adceaea3122b9b16cd71161e36ca6eb550ff8e6
Сергей Усков <q3mydoom@hotmail.com>
2021-11-01 12:51:39 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_Theme_4_Source_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            int MinValue = 0;
+            int MaxValue = 500000;
+            int rows = 12;
+            int cols = 4;
+            int NumberOfProfitMonth = 0;
+            int NumberOfworthMonth = 0;
+
+            int[] WorthMonth = new int[rows];
+            int[] WorthMonthSorted = new int[rows + 1];
+            int[] Profit = new int[rows];
+            int[,] table = new int[rows, cols];
+            string[] Month = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+
+            Random rand = new Random();
+
+            Console.WriteLine("   Месяц   |Доход, т.р. |Расход, т.р |Прибыль, т.р|");
+            for (i = 0; i < rows ; i++)
+            {
+                for (j = 0; j < cols; j++)
+                {
+                    table[i, j] = rand.Next(MinValue, MaxValue); //можно точно так же заполнить из 2х одномерных массивов, с заранее определёнными данными, но суть не меняет
+                    //первый столбец с номерами месяцев
+                    int ii = i;
+                    table[i, 0] = ii + 1;
+
+                    //последний столбец с прибылью (сумма 2 и 3 столбца)
+                    table[i, cols - 1] = table[i, cols - 3] - table[i, cols - 2];
+                    Profit[i] = table[i, cols - 1];
+
+                    Console.Write($"{table[i, j],10} | ");
+                }
+                //определение количества прибыльных
+                if (table[i, cols - 1] > 0)
+                {
+                    NumberOfProfitMonth++;
+                } else
+                {
+                    WorthMonth[i] = table[i, cols - 1];
+                    WorthMonthSorted[i] = table[i, cols - 1];
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Количество прибыльных месяцев: {NumberOfProfitMonth}");
+
+            //Три худших показателя:
+            Console.WriteLine($"Три худших месяца:");
+            Array.Sort(WorthMonthSorted);
+                                   
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < rows; j++)
+                {
+                    if (WorthMonthSorted[i] == Profit[j])
+                    {
+                        Console.Write($"{Month[j]}: {WorthMonthSorted[i]}  ");
+                    }
+                    if (WorthMonthSorted[i] == WorthMonthSorted[i + 1])
+                    {
+                        Console.Write($"Месяц {Month[j]} совпадает ");
+                    }
+                }
+            }
+
+                Console.WriteLine();
+
+            /*
+            //вывод прибыли
+            for (i = 0; i < rows; i++)
+            {
+                Console.Write($"{Profit[i]}  ");
+            }
+            */
+
+                Console.ReadKey();
+        }
+    }
+}
